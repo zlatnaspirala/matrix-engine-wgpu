5a18cd0363e6326e2e5120e923c00448798d0f63
zlatnaspirala <zlatnaspirala@gmail.com>
2026-02-26 21:48:48 +0100

    stable

--- public/examples.js
+++ public/examples.js
@@ -26318,13 +26318,47 @@ class BVHPlayerInstances extends _meshObjInstances.default {
     this.animationIndex = index;
     this.buildNodeChannelMap();
   };
+  composeTRS(t, q, s, out) {
+    const x = q[0],
+      y = q[1],
+      z = q[2],
+      w = q[3];
+    const x2 = x + x,
+      y2 = y + y,
+      z2 = z + z;
+    const xx = x * x2,
+      xy = x * y2,
+      xz = x * z2;
+    const yy = y * y2,
+      yz = y * z2,
+      zz = z * z2;
+    const wx = w * x2,
+      wy = w * y2,
+      wz = w * z2;
+    out[0] = (1 - (yy + zz)) * s[0];
+    out[1] = (xy + wz) * s[0];
+    out[2] = (xz - wy) * s[0];
+    out[3] = 0;
+    out[4] = (xy - wz) * s[1];
+    out[5] = (1 - (xx + zz)) * s[1];
+    out[6] = (yz + wx) * s[1];
+    out[7] = 0;
+    out[8] = (xz + wy) * s[2];
+    out[9] = (yz - wx) * s[2];
+    out[10] = (1 - (xx + yy)) * s[2];
+    out[11] = 0;
+    out[12] = t[0];
+    out[13] = t[1];
+    out[14] = t[2];
+    out[15] = 1;
+  }
   update(deltaTime) {
-    const frameTime = 1 / this.fps;
-    this.sharedState.timeAccumulator += deltaTime;
-    while (this.sharedState.timeAccumulator >= frameTime) {
-      this.sharedState.currentFrame = (this.sharedState.currentFrame + 1) % this._numFrames;
-      this.sharedState.timeAccumulator -= frameTime;
-    }
+    // const frameTime = 1 / this.fps;
+    // this.sharedState.timeAccumulator += deltaTime;
+    // while(this.sharedState.timeAccumulator >= frameTime) {
+    //   // this.sharedState.currentFrame = (this.sharedState.currentFrame + 1) % this._numFrames;
+    //   this.sharedState.timeAccumulator -= frameTime;
+    // }
     var inTime = this.getAnimationLength(this.glb.glbJsonData.animations[this.animationIndex]);
     if (this.sharedState.animationStarted == false && this.sharedState.emitAnimationEvent == true) {
       this.sharedState.animationStarted = true;
@@ -26869,13 +26903,47 @@ class BVHPlayer extends _meshObj.default {
     const numFrames = inputAccessor.count;
     return numFrames;
   }
+  composeTRS(t, q, s, out) {
+    const x = q[0],
+      y = q[1],
+      z = q[2],
+      w = q[3];
+    const x2 = x + x,
+      y2 = y + y,
+      z2 = z + z;
+    const xx = x * x2,
+      xy = x * y2,
+      xz = x * z2;
+    const yy = y * y2,
+      yz = y * z2,
+      zz = z * z2;
+    const wx = w * x2,
+      wy = w * y2,
+      wz = w * z2;
+    out[0] = (1 - (yy + zz)) * s[0];
+    out[1] = (xy + wz) * s[0];
+    out[2] = (xz - wy) * s[0];
+    out[3] = 0;
+    out[4] = (xy - wz) * s[1];
+    out[5] = (1 - (xx + zz)) * s[1];
+    out[6] = (yz + wx) * s[1];
+    out[7] = 0;
+    out[8] = (xz + wy) * s[2];
+    out[9] = (yz - wx) * s[2];
+    out[10] = (1 - (xx + yy)) * s[2];
+    out[11] = 0;
+    out[12] = t[0];
+    out[13] = t[1];
+    out[14] = t[2];
+    out[15] = 1;
+  }
   update(deltaTime) {
-    const frameTime = 1 / this.fps;
-    this.sharedState.timeAccumulator += deltaTime;
-    while (this.sharedState.timeAccumulator >= frameTime) {
-      this.sharedState.currentFrame = (this.sharedState.currentFrame + 1) % this._numFrames;
-      this.sharedState.timeAccumulator -= frameTime;
-    }
+    // const frameTime = 1 / this.fps;
+    // this.sharedState.timeAccumulator += deltaTime;
+    // while(this.sharedState.timeAccumulator >= frameTime) {
+    //   // this.sharedState.currentFrame = (this.sharedState.currentFrame + 1) % this._numFrames;
+    //   // this.sharedState.timeAccumulator -= frameTime;
+    // }
     const currentTime = performance.now() / this.animationSpeed - this.startTime;
     // const boneMatrices = new Float32Array(this.MAX_BONES * 16);
     if (this.glb.glbJsonData.animations && this.glb.glbJsonData.animations.length > 0) {
