73e1c6d89e2770f32d967da3f3c625057832c899
zlatnaspirala <zlatnaspirala@gmail.com>
2026-03-29 00:47:28 +0100

    Update cache.js

--- public/cache.js
+++ public/cache.js
@@ -9,7 +9,7 @@
  * for `cacheVersion`.
  * @param cacheVersion
  */
-var cacheVersion = 53;
+var cacheVersion = 54;
 // var prefix = 'matrix-engine-fohb';
 var prefix = 'matrix-engine-examples';
 var cacheName = prefix + cacheVersion;
