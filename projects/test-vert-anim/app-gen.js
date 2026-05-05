9eaa618c6800c1cd024fb54fc8f865feccb3218b
zlatnaspirala <zlatnaspirala@gmail.com>
2026-05-05 22:00:50 +0200

    tutorial for vertex anim from graph

--- projects/test-vert-anim/app-gen.js
+++ projects/test-vert-anim/app-gen.js
@@ -57,19 +57,7 @@ app.addLight();
 
   
 
-       // ME START FLOOR updatePosy
- setTimeout(() => {
-  app.getSceneObjectByName('FLOOR').position.SetY(-0.12000000000000001);
- }, 800);
- // ME END FLOOR updatePosy
- 
-         // ME START FLOOR updatePosz
- setTimeout(() => {
-  app.getSceneObjectByName('FLOOR').position.SetZ(-20);
- }, 800);
- // ME END FLOOR updatePosz
- 
-  // ME START SUB16
+       // ME START SUB16
  downloadMeshes({cube: "res/meshes/shapes/cube-SUB-16.obj"}, (m) => { 
    const texturesPaths = ['./res/textures/cube-g1-extra_low.png']; 
    app.addMeshObj({
@@ -119,6 +107,110 @@ app.addLight();
   app.getSceneObjectByName('SUB16').position.SetY(4.035);
  }, 800);
  // ME END SUB16 updatePosy
+ 
+ 
+       // ME START OCEAN addCube
+ downloadMeshes({cube: "./res/meshes/blender/cube.obj"}, (m) => { 
+   let texturesPaths = ['./res/meshes/blender/cube-g1-extra_low.png']; 
+   app.addMeshObj({
+     position: {x: 0, y: 0, z: -20}, rotation: {x: 0, y: 0, z: 0}, rotationSpeed: {x: 0, y: 0, z: 0},
+     texturesPaths: [texturesPaths],
+     name: 'OCEAN',
+     mesh: m.cube,
+     raycast: {enabled: true, radius: 2},
+     physics: {enabled: false, geometry: "Cube"}
+   }); 
+ }, {scale: [1, 1, 1]});  
+ // ME END OCEAN addCube
+ 
+
+       // ME START OCEAN updatePosx
+ setTimeout(() => {
+  app.getSceneObjectByName('OCEAN').position.SetX(-7.665);
+ }, 800);
+ // ME END OCEAN updatePosx
+ 
+    // ME START OCEAN updatePosy
+ setTimeout(() => {
+  app.getSceneObjectByName('OCEAN').position.SetY(7.245000000000001);
+ }, 800);
+ // ME END OCEAN updatePosy
+ 
+  // ME START OCEAN updateScale0
+ setTimeout(() => {
+  app.getSceneObjectByName('OCEAN').scale[0] = 2;
+ }, 800);
+ // ME END OCEAN updateScale0
+ 
+  // ME START OCEAN updateScale1
+ setTimeout(() => {
+  app.getSceneObjectByName('OCEAN').scale[1] = 2;
+ }, 800);
+ // ME END OCEAN updateScale1
+ 
+  // ME START OCEAN updateScale2
+ setTimeout(() => {
+  app.getSceneObjectByName('OCEAN').scale[2] = 2;
+ }, 800);
+ // ME END OCEAN updateScale2
+ 
+  // ME START FLOOR updatePosy
+ setTimeout(() => {
+  app.getSceneObjectByName('FLOOR').position.SetY(-1.2750000000000001);
+ }, 800);
+ // ME END FLOOR updatePosy
+ 
+ 
+       // ME START WAVE addCube
+ downloadMeshes({cube: "./res/meshes/blender/cube.obj"}, (m) => { 
+   let texturesPaths = ['./res/meshes/blender/cube-g1-extra_low.png']; 
+   app.addMeshObj({
+     position: {x: 0, y: 0, z: -20}, rotation: {x: 0, y: 0, z: 0}, rotationSpeed: {x: 0, y: 0, z: 0},
+     texturesPaths: [texturesPaths],
+     name: 'WAVE',
+     mesh: m.cube,
+     raycast: {enabled: true, radius: 2},
+     physics: {enabled: false, geometry: "Cube"}
+   }); 
+ }, {scale: [1, 1, 1]});  
+ // ME END WAVE addCube
+ 
+
+       // ME START WAVE updatePosy
+ setTimeout(() => {
+  app.getSceneObjectByName('WAVE').position.SetY(7.315);
+ }, 800);
+ // ME END WAVE updatePosy
+ 
+  // ME START WAVE updateScale0
+ setTimeout(() => {
+  app.getSceneObjectByName('WAVE').scale[0] = 2;
+ }, 800);
+ // ME END WAVE updateScale0
+ 
+  // ME START WAVE updateScale1
+ setTimeout(() => {
+  app.getSceneObjectByName('WAVE').scale[1] = 2;
+ }, 800);
+ // ME END WAVE updateScale1
+ 
+  // ME START WAVE updateScale2
+ setTimeout(() => {
+  app.getSceneObjectByName('WAVE').scale[2] = 2;
+ }, 800);
+ // ME END WAVE updateScale2
+ 
+  // ME START WAVE updatePosx
+ setTimeout(() => {
+  app.getSceneObjectByName('WAVE').position.SetX(7.839999999999991);
+ }, 800);
+ // ME END WAVE updatePosx
+ 
+  // ME START FLOOR updatePosz
+ setTimeout(() => {
+  app.getSceneObjectByName('FLOOR').position.SetZ(-19.96556934602543);
+ }, 800);
+ // ME END FLOOR updatePosz
  
  // [MAIN_REPLACE2]
 })
