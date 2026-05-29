d59f95293c80ff5be237d9143ce44693c06c5cea
zlatnaspirala <zlatnaspirala@gmail.com>
2026-05-29 20:14:46 +0200

    wip

--- public/cache.js
+++ public/cache.js
@@ -16,7 +16,7 @@
  * But resouces must be shared if both on same domain.
  */
 
-var cacheVersion = 41;
+var cacheVersion = 42;
 var prefix = 'matrix-engine-examples';
 var cacheName = prefix + cacheVersion;
 
