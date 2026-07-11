c60a14eecf8e7240a41d08ae78638d127c9f6400
zlatnaspirala <zlatnaspirala@gmail.com>
2026-07-11 17:13:37 +0200

    1.17.0 zombi worls (hang3d series)
    Improving FPCamera

--- public/cache.js
+++ public/cache.js
@@ -16,7 +16,7 @@
  * But resouces must be shared if both on same domain.
  */
 
-var cacheVersion = 91;
+var cacheVersion = 92;
 var prefix = 'matrix-engine-examples';
 var cacheName = prefix + cacheVersion;
 
