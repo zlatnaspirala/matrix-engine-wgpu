505647ccdf0f85e5936f2c1dcfa7a28f51ca3e4e
zlatnaspirala <zlatnaspirala@gmail.com>
2026-08-02 20:24:01 +0200

    1.18.1

--- public/cache.js
+++ public/cache.js
@@ -16,7 +16,7 @@
  * But resouces must be shared if both on same domain.
  */
 
-var cacheVersion = 176;
+var cacheVersion = 179;
 var prefix = 'matrix-engine-examples';
 var cacheName = prefix + cacheVersion;
 
