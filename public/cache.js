c5d9cdccd89cd99050b9ba4dc68d8596f8a95151
zlatnaspirala <zlatnaspirala@gmail.com>
2026-08-23 21:57:14 +0200

    test video in github readme

--- public/cache.js
+++ public/cache.js
@@ -16,7 +16,7 @@
  * But resouces must be shared if both on same domain.
  */
 
-var cacheVersion = 184;
+var cacheVersion = 185;
 var prefix = 'matrix-engine-examples';
 var cacheName = prefix + cacheVersion;
 
